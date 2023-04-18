a6b5f724effa3679ce087f94419845fee3b6b1d3
dunham-mike <mdunham@summitps.org>
2023-04-17 18:10:05 -0700

    Add function to switch to a specified school.

--- spswarehouse/powerschool.py
+++ spswarehouse/powerschool.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 # from selenium.webdriver.support.ui import Select
 # from selenium import webdriver
@@ -7,6 +8,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+ADMIN_LOGIN_PAGE_PATH = 'admin/pw.html'
+ADMIN_HOME_PAGE_PATH = 'admin/home.html'
+ADMIN_URL_SCHEME = 'https://'
 
 def log_into_powerschool_admin(driver: WebDriver, 
                                username: str, 
@@ -26,8 +32,7 @@ def log_into_powerschool_admin(driver: WebDriver,
         None: Selenium will log you into PowerSchool Admin
     """
     logging.info("Create webpage URL for PowerSchool Admin")
-    host_file_page_type = '/admin/pw.html'
-    host_full = host + host_file_page_type
+    host_full = host + "/" + ADMIN_LOGIN_PAGE_PATH
     logging.info(f"The webpage url is: {host_full}")
     
     logging.info("Go to webpage URL for PowerSchool Admin")
@@ -48,8 +53,84 @@ def log_into_powerschool_admin(driver: WebDriver,
     logging.info("Find the password field within the HTML page")
     elem = driver.find_element_by_id('fieldPassword')
     
-    logging.info("Type your PowerSchool CA password")
+    logging.info("Type your PowerSchool password")
     elem.send_keys(password)
     
     logging.info("Press enter to submit your credentials and complete your login.")
     elem.send_keys(Keys.RETURN)
+
+def get_current_domain(driver: WebDriver):
+    return driver.current_url[8:].split("/",1)[:1][0]
+
+def get_current_path(driver: WebDriver):
+    return driver.current_url[8:].split("/",1)[1:][0]
+
+def ensure_on_desired_path(driver: WebDriver, desired_path: str):
+    current_path = get_current_path(driver)
+
+    logging.info(f"The current path is: {current_path}")
+
+    if(current_path == desired_path):
+        logging.info("The current path is the desired path. No action taken.")
+    else:
+        logging.info(f"This does not match {desired_path}, so going to that path")
+        driver.get('https://' + get_current_domain(driver) + "/" + desired_path)
+        logging.info(f"Moved to {desired_path}")
+
+def switch_to_school(driver: WebDriver, 
+                    school_name: str):
+    try:
+        ensure_on_desired_path(driver, ADMIN_HOME_PAGE_PATH)
+
+        logging.info("Waiting for School Picker")
+        elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'school_picker_adminSchoolPicker_toggle_btn')))
+        logging.info("School Picker found. Click it.")
+
+        elem.click()
+
+        time.sleep(1)
+
+        logging.info("Waiting for School Search Field")
+        elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'schoolSearchField_value')))
+        logging.info("Found School Search Field. Typing in school name.")
+
+        elem.send_keys(school_name)
+
+        time.sleep(1)
+
+        logging.info("Looking for first school in list")
+        elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//ul[@id='school_choices']/li[1]")))
+
+        logging.info("Found first school in results list. Clicking.")
+        elem.click()
+        logging.info("Click. Waiting for page to refresh.")
+        time.sleep(1)
+
+        elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'school_picker_adminSchoolPicker_toggle_btn')))
+
+        elem.click()
+        time.sleep(1)
+
+        selected_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.list-item.selectable.selected')))
+
+        school_element_text = selected_element.find_element(By.XPATH, ".//div").text
+
+        actions = ActionChains(driver)
+
+        if(school_name in school_element_text):
+            logging.info(f"Found the {school_name} in the school element text. Success!")
+
+            logging.info(f"Pressing escape.")
+            actions.send_keys(Keys.ESCAPE).perform()
+
+            return True
+        else:
+            logging.info(f"Did not find {school_name} in the school element text. Returning False.")
+
+            logging.info(f"Pressing escape.")
+            actions.send_keys(Keys.ESCAPE).perform()
+
+            return False
+    except:
+        logging.info(f"Selenium exception. Returning False.")
+        return False
