0fb591a98fcd55a859e3b5c7b579cccf2d9b6869
dunham-mike <mdunham@summitps.org>
2023-04-19 16:54:25 -0700

    Refactor first CALPADS report function to use reusable helper functions.

--- spswarehouse/powerschool/calpads.py
+++ spswarehouse/powerschool/calpads.py
@@ -14,40 +14,50 @@ def download_calpads_report_for_school(driver: WebDriver, school_full_name: str,
     if(report_name == "Student Incident Records (SINC)"):
         download_calpads_report_for_school_student_incident_records_sinc(driver, school_full_name, report_name, file_postfix, destination_directory_path, 
                                                              report_start_date=report_parameters['report_start_date'],
-                                                             report_end_date=report_parameters['report_end_date'],
-                                                             run_validations=report_parameters['run_validations'])
+                                                             report_end_date=report_parameters['report_end_date']
+                                                             )
+    # elif(report_name == "Student Incident Results Records (SIRS)"):
+    #     download_calpads_report_for_school_student_incident_results_records_sirs(driver, school_full_name, report_name, file_postfix, destination_directory_path, 
+    #                                                          report_start_date=report_parameters['report_start_date'],
+    #                                                          report_end_date=report_parameters['report_end_date']
+    #                                                          )
     else:
         raise Exception("CALPADS report name not supported")
 
+# TODO: Move these helper functions to the main powerschool.py file
+def powerschool_report_helper_type_in_element_by_id(driver: WebDriver, element_id: str, input_to_type: str):
+    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, element_id)))
+    elem.clear()
+    elem.send_keys(input_to_type)
+
+def powerschool_report_helper_select_visible_text_in_element_by_id(driver: WebDriver, element_id: str, text_to_select: str):
+    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, element_id)))
+    select = Select(elem)
+    select.select_by_visible_text(text_to_select)
+
+def powerschool_report_helper_click_element_by_id(driver: WebDriver, element_id: str):
+    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, element_id)))
+    elem.click()
+
 def download_calpads_report_for_school_student_incident_records_sinc(driver: WebDriver, school_full_name: str, report_name: str, file_postfix: str, destination_directory_path: str, 
-                                                        report_start_date: str, report_end_date: str, run_validations: bool):
+                                                        report_start_date: str, report_end_date: str):
     
     navigate_to_specific_state_report(driver, "Student Incident Records (SINC)")
     
     # Enter specific parameters for this report
-    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'reportStartDate')))
-    elem.clear()
-    elem.send_keys(report_start_date)
-
-    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'reportEndDate')))
-    elem.clear()
-    elem.send_keys(report_end_date)
-
-    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'reportMode')))
-    select = Select(elem)
-    select.select_by_visible_text("Submission mode")
-
-    time.sleep(1)
-
-    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'bypassValidation')))
-    select = Select(elem)
-    if run_validations:
-        select.select_by_visible_text("No")
-    else:
-        select.select_by_visible_text("Yes")
+    powerschool_report_helper_type_in_element_by_id(driver, 'reportStartDate', report_start_date)
+    powerschool_report_helper_type_in_element_by_id(driver, 'reportEndDate', report_end_date)
+    powerschool_report_helper_select_visible_text_in_element_by_id(driver, 'reportMode', 'Submission mode') # Only 'Submission mode' is supported by this tool
+    time.sleep(1) # Give page time to react
+    powerschool_report_helper_select_visible_text_in_element_by_id(driver, 'bypassValidation', 'Yes')
 
     # Submit report
-    elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, 'submitReportSDKRuntimeParams')))
-    elem.click()
+    powerschool_report_helper_click_element_by_id(driver, 'submitReportSDKRuntimeParams')
 
+    # Download report zipfile
     download_latest_report_from_report_queue(driver, destination_directory_path, file_postfix)
+
+
+# def download_calpads_report_for_school_student_incident_results_records_sirs(driver: WebDriver, school_full_name: str, report_name: str, file_postfix: str, destination_directory_path: str, 
+#                                                         report_start_date: str, report_end_date: str):
+#     return False
