9f59577cb8081897c21984b87d82dfcce38341f1
dunham-mike <mdunham@summitps.org>
2023-04-19 14:30:16 -0700

    Refactor to create generic report pulling function.

--- spswarehouse/powerschool/calpads.py
+++ spswarehouse/powerschool/calpads.py
@@ -1,5 +1,5 @@
 import time
-from spswarehouse.powerschool.powerschool import navigate_to_specific_state_report, download_latest_report_from_report_queue
+from spswarehouse.powerschool.powerschool import navigate_to_specific_state_report, download_latest_report_from_report_queue, switch_to_school
 
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait, Select
@@ -8,7 +8,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-def download_calpads_student_incident_records_for_school(driver: WebDriver, report_start_date: str, report_end_date: str, destination_directory_path: str, file_postfix: str, run_validations=True):
+def download_calpads_report_for_school(driver: WebDriver, school_full_name: str, report_name: str, file_postfix: str, destination_directory_path: str, report_parameters: dict):
+    switch_to_school(driver, school_full_name)
+
+    if(report_name == "Student Incident Records (SINC)"):
+        download_calpads_report_for_school_student_incident_records_sinc(driver, school_full_name, report_name, file_postfix, destination_directory_path, 
+                                                             report_start_date=report_parameters['report_start_date'],
+                                                             report_end_date=report_parameters['report_end_date'],
+                                                             run_validations=report_parameters['run_validations'])
+    else:
+        raise Exception("CALPADS report name not supported")
+
+def download_calpads_report_for_school_student_incident_records_sinc(driver: WebDriver, school_full_name: str, report_name: str, file_postfix: str, destination_directory_path: str, 
+                                                        report_start_date: str, report_end_date: str, run_validations: bool):
+    
     navigate_to_specific_state_report(driver, "Student Incident Records (SINC)")
     
     # Enter specific parameters for this report
