fb366ad624953485f985527d31b92b1319228d0f
howard_shen <hshen@summitps.org>
2023-10-24 18:56:13 -0400

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spswarehouse",
-    version="0.5.1",
+    version="0.5.2",
     author="Summit Public Schools; Harry Li Consulting, LLC",
     author_email="warehouse@summitps.org",
     description="Summit Public Schools Snowflake warehouse",
